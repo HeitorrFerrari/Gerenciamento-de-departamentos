ee42c435946affa824130c5031e9893cb53aec11
duudafs <dudascomparin@gmail.com>
2026-03-22 20:20:50 -0300

    model finalizada

--- src/main/java/com/mh/GerenciamentoDeDepartamentos/models/DepartamentoModel.java
+++ src/main/java/com/mh/GerenciamentoDeDepartamentos/models/DepartamentoModel.java
@@ -1,4 +1,48 @@
 package com.mh.GerenciamentoDeDepartamentos.models;
 
+import jakarta.persistence.*;
+
+@Entity(name = "departamento")
+@Table(name = "tb_departamento")
 public class DepartamentoModel {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "nome_departamento")
+    private String nome;
+
+    @Column(name = "localizacao_departamento")
+    private String localizacao;
+
+    public DepartamentoModel(Long id, String nome, String localizacao) {
+        this.id = id;
+        this.nome = nome;
+        this.localizacao = localizacao;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getLocalizacao() {
+        return localizacao;
+    }
+
+    public void setLocalizacao(String localizacao) {
+        this.localizacao = localizacao;
+    }
 }
