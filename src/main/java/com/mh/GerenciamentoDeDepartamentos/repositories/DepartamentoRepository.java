db85d8a833d340100d7c961da5c1d2f7c5029050
duudafs <dudascomparin@gmail.com>
2026-03-22 20:23:17 -0300

    repository finalizado

--- src/main/java/com/mh/GerenciamentoDeDepartamentos/repositories/DepartamentoRepository.java
+++ src/main/java/com/mh/GerenciamentoDeDepartamentos/repositories/DepartamentoRepository.java
@@ -1,8 +1,10 @@
 package com.mh.GerenciamentoDeDepartamentos.repositories;
 
 
+import com.mh.GerenciamentoDeDepartamentos.models.DepartamentoModel;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface DepartamentoRepository {
+public interface DepartamentoRepository extends JpaRepository<DepartamentoModel, Long> {
 }
